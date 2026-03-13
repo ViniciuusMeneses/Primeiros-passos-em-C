5256dfb901adca97510cf70bd423bd065dcd419e
Vinicius Meneses <138729617+ViniciuusMeneses@users.noreply.github.com>
2026-03-13 09:13:31 -0300

    Create Multiplicação sem *.c

---
+++ Atividades/Multiplicação sem *.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, res = 0;
+    
+    printf("Insira o primeiro numero: ");
+    scanf("%d", &num1);
+    
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num2);
+    
+    int i = 0;
+    while (i < num2) {
+        res += num1;  // soma num1 repetidamente
+        i++;
+    }
+    
+    printf("O resultado da multiplicacao e: %d\n", res);
+    
+    return 0;
+}
