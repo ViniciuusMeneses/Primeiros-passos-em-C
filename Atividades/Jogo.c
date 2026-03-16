8d0053b3c59809e069b264b09f24ca2bb95125e0
Vinicius Meneses <138729617+ViniciuusMeneses@users.noreply.github.com>
2026-03-16 09:41:31 -0300

    Create Jogo.c

---
+++ Atividades/Jogo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+	int jogo;
+	
+	printf("-----Steam indie-----\n");
+	printf("Jogo das pergunstas (1)\n");
+	printf("Cobra na caixa (2)\n");
+	printf("Gousmas War (3)\n");
+	scanf("%d", &jogo);
+	if(jogo == 1){
+		int acertos
+		
+		printf("Primeira pergunta\n");
+		printf("Qual o unico time do norte a disputa uma Libertadores?\n")
+		printf("Paysandu (1)\n")
+		printf("Manaus (2)\n")
+		Printf("Goias (3)\n")
+		Printf("Remo (4)\n")
+		scanf("%d", &acertos)
+		
+		
+	} else if( jogo == 2){
+		printf("Calma, to fazendo pae");
+	} else if(jogo == 3){
+		printf("calma, to fazendo pae");
+	} else{
+		printf("TO FAZENDO EGUA");
+	}
+	
+	
+	
+	return 0;
+}
