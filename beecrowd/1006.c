7dd545aeac076ee42fe8038484d67677934441df
Vinicius Meneses <138729617+ViniciuusMeneses@users.noreply.github.com>
2026-04-19 20:13:10 -0300

    Create 1006.c

---
+++ beecrowd/1006.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+   double n1, n2, n3;
+   scanf("%lf", &n1);
+   scanf("%lf", &n2);
+   scanf("%lf", &n3);
+   printf("MEDIA = %.1lf\n", (n1*2 + 3*n2 + n3*5)/10);
+    return 0;
+}
