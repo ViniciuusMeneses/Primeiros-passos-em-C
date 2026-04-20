9aa6a04ce0683cd56905d7a96aa16ad5bd769760
Vinicius Meneses <138729617+ViniciuusMeneses@users.noreply.github.com>
2026-04-19 21:13:02 -0300

    Add program to calculate salary based on hours worked

---
+++ beecrowd/1008.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+   int n, h;
+   float s;
+   scanf("%d", &n);
+   scanf("%d", &h);
+   scanf("%f", &s);
+   printf("NUMBER = %d\n", n);
+   printf("SALARY = U$ %.2f\n", s*h );
+    return 0;
+}
