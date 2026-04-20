02ee92946ccbbf0b8c7448ef073ddc609207dbfd
Vinicius Meneses <138729617+ViniciuusMeneses@users.noreply.github.com>
2026-04-20 12:11:06 -0300

    Create 1012.c

---
+++ beecrowd/1012.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    double b, h, r;
+    
+    scanf("%lf %lf %lf", &b, &h, &r);
+    printf("TRIANGULO: ");
+    printf("CIRCULO: ");
+    printf("TRAPEZIO: ");
+    printf("QUADRADO: ");
+    printf("RETANGULO: ");
+
+    return 0;
+}
